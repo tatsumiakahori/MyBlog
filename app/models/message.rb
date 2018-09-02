995a7925aed8f4721af37f31c42f9415462f7dc4
EC2 Default User <ec2-user@ip-172-31-27-114.us-east-2.compute.internal>
2018-09-02 12:20:05 +0000

    new-column

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,7 @@
 class Message < ApplicationRecord
-    validates :title , length:(maximum: 32) , presence: true
-    validates :message , length:(maximum: 256) , presence: true
-    validates :name , length:(maximum: 32)
+    validates :title , length:{maximum: 20} , presence: true
+    validates :message , length:{maximum: 256} , presence: true
+    validates :name , length:{maximum: 20}
+    validates :age , length:{maximum: 3}
+
 end
---
+++ db/migrate/20180902033147_add_age_to_message.rb
@@ -0,0 +1,5 @@
+class AddAgeToMessage < ActiveRecord::Migration[5.2]
+  def change
+    add_column :messages, :age, :integer
+  end
+end
